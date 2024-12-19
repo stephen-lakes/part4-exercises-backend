ac1f4aeb629731349475be0400f47f744b4390e9
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-19 08:10:45 +0100

    implement blog schema

---
+++ models/blog.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+blogSchema = mongoose.Schema({});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
