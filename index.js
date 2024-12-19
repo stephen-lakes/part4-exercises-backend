a88a709c9ff4170997dcc78138828e761c17b3ca
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-19 08:05:47 +0100

    test blog schema
    insert record
    
    tested the endpoints

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const Blog = require("./models/note")
 
-const Blog = mongoose.model("Blog", blogSchema);
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// });
+
+// const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
