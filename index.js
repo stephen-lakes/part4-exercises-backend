58b7e97c5feea167ad465972530a3858a2e0c2fd
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 17:13:04 +0100

    connect to database and set up get and post routes

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-// const app = require("./app");
-// const config = require("./utils/config");
-// const logger = require("./utils/logger");
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-// app.listen(config.PORT, () => {
-//   logger.info(`Server running on port ${config.PORT}`);
-// });
-
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
 require("dotenv").config();
 
-const Blog = require("./models/note")
-
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-// });
-
-// const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
+const Blog = require("./models/blog");
 
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -43,7 +20,6 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
