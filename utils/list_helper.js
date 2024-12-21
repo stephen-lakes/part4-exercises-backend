bb2d66b7f3200ddf2038b06e333c0e5198770d49
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-22 00:43:45 +0100

    add test for favoriteBlogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,4 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  let fav;
+  let highest = 0;
+  blogs.forEach((blog) => {
+    if (blog.likes > highest) {
+      fav = { title: blog.title, author: blog.author, likes: blog.likes };
+
+      highest = blog.likes;
+    }
+  });
+  return fav;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
