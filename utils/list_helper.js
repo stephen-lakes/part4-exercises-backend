d5499b806ff96d32901e66a10a64b0226ed2248a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-22 00:16:22 +0100

    add test for total likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const getSum = () => {
+    let sum = 0;
+    blogs.forEach((blog) => {
+      sum = sum + blog.likes;
+    });
+    return sum;
+  };
+
+  return blogs.length === 0 ? 0 : blogs.length === 1 ? blogs[0].likes : getSum();
+};
+
+module.exports = { dummy, totalLikes };
