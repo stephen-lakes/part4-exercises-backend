75ae165421e116fee316350d76591f02042cfdad
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-22 00:17:19 +0100

    enhancement: optimized test
    make test function cleaner

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,15 +3,7 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  const getSum = () => {
-    let sum = 0;
-    blogs.forEach((blog) => {
-      sum = sum + blog.likes;
-    });
-    return sum;
-  };
-
-  return blogs.length === 0 ? 0 : blogs.length === 1 ? blogs[0].likes : getSum();
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
 module.exports = { dummy, totalLikes };
