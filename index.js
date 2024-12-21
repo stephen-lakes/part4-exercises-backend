7838cbd6af39e6721866e16463d041e35a0c4b0e
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-21 23:01:11 +0100

    chore: clean up

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ const logger = require("./utils/logger");
 
 require("dotenv").config();
 
-const Blog = require("./models/blog");
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
