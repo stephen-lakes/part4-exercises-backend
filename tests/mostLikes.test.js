00055f76b674d4071641e97c9221618ae2667e9f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-24 16:44:35 +0100

    feat: Setup test structure for mostLikes
    - import the necessary functions from node:test and node:assert
    - use the describe function to group nostLikes test
    - Include test case for empty list
    - Include test case to calcaulte most likes

---
+++ tests/mostLikes.test.js
@@ -0,0 +1,9 @@
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
+
+describe("most likes", () => {
+  test("of empty list", () => {});
+
+  test("of most likes is calculates right", () => {});
+});
