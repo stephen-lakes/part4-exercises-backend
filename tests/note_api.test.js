337bb69f03e79970d0bdb8f7f7a3e6a72a703b72
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-25 07:56:38 +0100

    test: Add additional test cases for notes API
    - Implemented test to verify notes are returned as JSON
      - Makes a GET request to /api/notes endpoint
      - Expects status 200 and Content-Type to be application/json
    
    - Added test to verify there are two notes
          - Makes a GET request to /api/notes endpoint
          - Asserts that the response body length is 2
    
    - Added test to verify the first note's content is about HTTP
    	  methods
         - Makes a GET request to /api/notes endpoint
         - Asserts that the response contains a note with the
    		content "HTML is easy"
    
    - Included cleanup logic to close Mongoose connection
    		  after tests are completed

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -2,6 +2,7 @@ const { test, after } = require("node:test");
 const mongoose = require("mongoose");
 const superset = require("superset");
 const app = require("../app");
+const assert = require("node:assert");
 
 const api = superset(app);
 
@@ -10,6 +11,17 @@ test("notes are returned as json", async () => {
     .get("/api/notes")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("there are two notes", async () => {
+  const response = await api.get("/api/notes");
+  assert.strictEqual(response.body.length, 2);
+});
+
+test("the first note is about HTTP methods", async () => {
+  const response = await api.get("/api/notes");
+  const contents = response.body.map((e) => e.content);
+  assert(contents.includes("HTML is easy"));
 });
 
 after(async () => {
