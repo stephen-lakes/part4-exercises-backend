d036946f2cd5ed0e3ee42fdc1e7cf21721600b64
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-26 21:37:48 +0100

    Remove all trycatch blocks
    allow express-async-errors to handle errors automatically

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,53 +2,33 @@ const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const logger = require("../utils/logger");
 
-notesRouter.get("/", async (request, response, next) => {
-  try {
-    const notes = await Note.find({});
-    response.json(notes);
-  } catch (error) {
-    logger.error("Error fetching notes:", error.message);
-    next(error);
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
+});
+
+notesRouter.get("/:id", async (request, response) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
 });
 
-notesRouter.get("/:id", async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id);
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end();
-    }
-  } catch (error) {
-    logger.error("Error fetching note:", error.message);
-    next(error);
-  }
-});
-
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
-  try {
-    const savedNote = await note.save();
-    response.status(201).json(savedNote);
-  } catch (error) {
-    logger.error("Error fetching notes:", error.message);
-    next(error);
-  }
+  const savedNote = await note.save();
+  response.status(201).json(savedNote);
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
-  try {
-    await Note.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } catch (error) {
-    logger.error("Error deleting note:", error.message);
-    next(error);
-  }
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 notesRouter.put("/:id", async (request, response, next) => {
@@ -57,16 +37,12 @@ notesRouter.put("/:id", async (request, response, next) => {
     content: body.content,
     important: body.important,
   };
-
-  try {
-    const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
-      new: true,
-    });
-    response.json(updatedNote);
-  } catch (error) {
-    logger.error("Error updating note", error.message);
-    next(error);
-  }
+  
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+  });
+  response.json(updatedNote);
+  
 });
 
 module.exports = notesRouter;
