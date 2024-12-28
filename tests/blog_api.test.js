aadca6bce35f05c9fce8dd652f446261f67bb135
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-28 02:54:25 +0100

    setup test for blogs endpoints

---
+++ tests/blog_api.test.js
@@ -0,0 +1,40 @@
+const { test, beforeEach, after } = require("node:test");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+
+const app = require("../app");
+const Blog = require("../models/blog");
+
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "Introduction to ExpressJS API",
+    author: "Stephen Oluyomi",
+    url: "https://www.stephen-dev.com/blogs/express-api",
+    likes: 10000,
+    id: "676d9cb624adfda4dbbe696f",
+  },
+  {
+    title: "How to Build a Blog with the Ghost API and Next.js",
+    author: "Rajdeep Singh",
+    url: "https://www.freecodecamp.org/news/build-a-blog-website-with-ghost-api-and-nextjs/",
+    likes: 8500,
+    id: "676d9d1f24adfda4dbbe6972",
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of initialBlogs) {
+    let blogObject = new Blog(blog);
+    blogObject.save();
+  }
+});
+
+test("the correct amount of blog posts are returned in the JSON format", async () => {});
+
+after(async () => {
+  await mongoose.connection.close();
+});
