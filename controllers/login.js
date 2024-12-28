a76ad96bb97d8b2b5bb4d92f7760c8b406c19cfe
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-28 11:01:21 +0100

    feat: Add token validation to login handler
    - Implemented function to extract token from authorization header
      - Checks for "Bearer" token format and returns the token string
    - Added token validation in login route
      - Decodes and verifies token using JWT and secret
      - Returns status 401 if token is invalid
      - Finds user by ID decoded from toke
    - Ensures secure login process by verifying token before checking username and password

--- controllers/login.js
+++ controllers/login.js
@@ -5,10 +5,23 @@ require("dotenv").config();
 
 const User = require("../models/user");
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer "))
+    return authorization.replace("Bearer", "");
+
+  return null;
+};
+
 loginRouter.post("/", async (request, response) => {
   const { username, password } = request.body;
 
-  const user = await User.findOne({ username });
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if(!decodedToken.id) return response.status(401).json({error: "token invalid"})
+
+  const user = await User.findById(decodedToken.id)
+
+  // const user = await User.findOne({ username });
 
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash);
