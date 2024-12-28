0f1adc993512f279d2b1aca91c53a4cc59f0b32c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-28 07:39:03 +0100

    feat: Add POST /api/users route for creating new users
    - Implemented route to handle POST requests to /api/users
      - Extracts username, name, and password from request body
      - Hashes password using bcrypt with a salt round of 10
      - Creates new user with hashed password and saves to the database
      - Responds with 201 Created status and JSON of saved user
    - Included error handling to log and manage errors using logger and error-handling middleware

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const usersRouter = require("express").Router();
+const bcrypt = require("bcrypt");
 
 const logger = require("../utils/logger");
 const User = require("../models/user");
@@ -9,6 +10,27 @@ usersRouter.get("/", async (request, response, next) => {
     response.status(200).json(users);
   } catch (error) {
     logger.error("Error fetching users", error);
+    next(error);
+  }
+});
+
+usersRouter.post("/", async (request, response, next) => {
+  const { username, name, password } = request.body;
+  const saltRound = 10;
+  const passwordHash = await bcrypt.hash(password, saltRound);
+
+  try {
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    response.status(201).json(savedUser);
+  } catch (error) {
+    logger.info("Error adding new user", error);
     next(error);
   }
 });
