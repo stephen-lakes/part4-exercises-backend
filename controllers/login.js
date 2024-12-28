98410e7aeb91ca37fae6fc1c4c9f468e306a8fb0
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-28 10:47:02 +0100

    import the correct module
    import user model from models/user.js

--- controllers/login.js
+++ controllers/login.js
@@ -3,12 +3,12 @@ const bcrypt = require("bcrypt");
 const loginRouter = require("express").Router();
 require("dotenv").config();
 
-const User = require("./users");
+const User = require("../models/user");
 
 loginRouter.post("/", async (request, response) => {
   const { username, password } = request.body;
 
-  const user = User.findOne({ username });
+  const user = await User.findOne({ username });
 
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash);
