530b22050932c8533017b5f77e4c6d497752d462
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-28 13:35:40 +0100

    fix: Enhance error handling to provide clean validation error messages
    - Modified error handling middleware to remove prefix "User validation
      failed"
      - Extracts and returns only specific validation error messages
    - Ensures clearer and more user-friendly error feedback for
          validation issues

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,7 +18,8 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+    const messages = Object.values(error.errors).map(err => err.message)
+    return response.status(400).json({ error: messages.join('. ') });
   } else if (
     error.name === "MongoServerError" &&
     error.message.includes("E11000 duplicate key error")
