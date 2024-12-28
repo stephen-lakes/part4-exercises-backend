afefff02b5cc4ddad8d220b51647aa80eade04ad
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-28 09:40:22 +0100

    feat: Associate new notes with users during creation
    - Updated POST /api/notes route to link notes to users
      - Finds user by userId from request body
      - Creates new note with user association
      - Saves note and updates user's notes array with the note ID
      - Ensures data consistency by linking notes to their creators

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 const logger = require("../utils/logger");
 
 notesRouter.get("/", async (request, response) => {
@@ -18,11 +19,16 @@ notesRouter.get("/:id", async (request, response) => {
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
@@ -37,12 +43,11 @@ notesRouter.put("/:id", async (request, response, next) => {
     content: body.content,
     important: body.important,
   };
-  
+
   const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
     new: true,
   });
   response.json(updatedNote);
-  
 });
 
 module.exports = notesRouter;
