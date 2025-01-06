ff010ec5e8362817ba5c15c2c7d93c1b59cb0408
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-06 16:08:31 +0100

    use jest for testing

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -3,20 +3,32 @@ const assert = require("node:assert");
 
 const reverse = require("../utils/for_testing").reverse;
 
-test("reverse of a", () => {
-  const result = reverse("a");
+// test("reverse of a", () => {
+//   const result = reverse("a");
 
-  assert.strictEqual(result, "a");
-});
+//   assert.strictEqual(result, "a");
+// });
+
+// test("reverse of react", () => {
+//   const result = reverse("react");
+
+//   assert.strictEqual(result, "tcaer");
+// });
+
+// test("reverse of saippuakauppias", () => {
+//   const result = reverse("saippuakauppias");
+
+//   assert.strictEqual(result, "saippuakauppias");
+// });
 
 test("reverse of react", () => {
   const result = reverse("react");
 
-  assert.strictEqual(result, "tcaer");
+  expect(result).toBe("tcaer");
 });
 
-test("reverse of saippuakauppias", () => {
-  const result = reverse("saippuakauppias");
+test("reverse of releveler", () => {
+  const result = reverse("releveler");
 
-  assert.strictEqual(result, "saippuakauppias");
+  expect(result).toBe("releveler");
 });
